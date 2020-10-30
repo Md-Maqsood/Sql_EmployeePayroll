fcdd9635813e3bd3e9506707b928365f2fc0b747
maqsood <maqsoodhamd37@gmail.com>
2020-10-30 07:35:00 +0530

    [Maqsood].update: added the feture to retrieve salary data for particular as well as employees joining in particular date range

--- Employee_Payroll.sql
+++ Employee_Payroll.sql
@@ -14,3 +14,19 @@ start date not null,
 primary key (id)
 );
 
+#UseCase3
+
+insert into employee_payroll(name, salary, start) values
+('Bill', 10000000.00, '2018-01-03'),
+('Terisa', 20000000.00, '2018-11-03'),
+('Charlie', 30000000.00, '2020-05-21');
+
+#UseCase4
+
+select * from employee_payroll;
+
+#UseCase5
+
+select salary from employee_payroll where name='Bill';
+select * from employee_payroll where start between cast('2018-01-01' as date) and date(now());
+
