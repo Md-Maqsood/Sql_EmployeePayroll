e7e3d1f2afedb8fc10b62e551f4e1b5151eaa5e9
maqsood <maqsoodhamd37@gmail.com>
2020-10-31 06:44:01 +0530

    [Maqsood].update: implemented the er diagram into payroll service database, did again uc7, solved uc11

--- Employee_Payroll.sql
+++ Employee_Payroll.sql
@@ -7,7 +7,7 @@ use payroll_service;
 #UseCase2
 
 create table employee_payroll(
-id int unsigned not null auto_increment,cd
+id int unsigned not null auto_increment,
 name varchar(150) not null,
 salary double not null,
 start date not null,
@@ -61,4 +61,79 @@ alter table employee_payroll add net_pay double not null after tax;
 update employee_payroll set department='Sales' where name='Terisa';
 insert into employee_payroll (name, department, gender, basic_pay, deductions, taxable_pay, tax, net_pay, start) values
 ('Terisa', 'Marketing', 'F', 30000000.00, 10000000.00, 20000000.00, 5000000.00, 15000000.00, '2018-11-03');
+
+#UseCase11
+
+create table company(
+company_id int unsigned not null auto_increment,
+company_name varchar(150) not null,
+primary key(company_id)
+);
+
+insert into company (company_name) values ('Capgemini');
+insert into company (company_name) values ('Bridgelabs');
+
+create table employee(
+employee_id int unsigned not null auto_increment,
+name varchar(150) not null,
+company_id int unsigned not null,
+gender char(1),
+address varchar(250) not null,
+phone_number varchar(150) not null,
+start date not null, 
+primary key (employee_id),
+foreign key (company_id) references company(company_id)
+);
+
+alter table employee alter address set default 'ABC';
+
+create table department(
+department_id int unsigned not null auto_increment,
+department_name varchar(150) not null,
+primary key(department_id)
+);
+
+create table employee_department(
+employee_id int unsigned not null,
+department_id int unsigned not null,
+foreign key (employee_id) references employee(employee_id),
+foreign key (department_id) references department(department_id)
+);
+
+create table payroll(
+employee_id int unsigned not null,
+basic_pay double not null,
+deductions double not null,
+taxable_income double not null,
+tax double not null,
+net_pay double not null,
+foreign key (employee_id) references employee(employee_id)
+);
+
+insert into employee (name, company_id, gender, phone_number) values ('Bill', 1, 'M', '7894561232');
+insert into employee (name, company_id, gender, phone_number) values ('Charlie', 1, 'M', '7894561232');
+insert into employee (name, company_id, gender, phone_number) values ('Terisa', 1, 'F', '9894561242');
+
+insert into department (department_name) values ('Sales');
+insert into department (department_name) values ('Marketing');
+
+insert into employee_department (employee_id, department_id) values (1, 2);
+insert into employee_department (employee_id, department_id) values (2, 1);
+insert into employee_department (employee_id, department_id) values (3, 1);
+insert into employee_department (employee_id, department_id) values (3, 2);
+
+insert into payroll (employee_id, basic_pay, deductions, taxable_income, tax, net_pay) values 
+(1, 10000000.00, 2000000.00,8000000.00,1000000.00,7000000.00);
+insert into payroll (employee_id, basic_pay, deductions, taxable_income, tax, net_pay) values 
+(2, 20000000.00, 4000000.00,16000000.00,3000000.00,13000000.00);
+insert into payroll (employee_id, basic_pay, deductions, taxable_income, tax, net_pay) values 
+(3, 30000000.00, 10000000.00,2000000.00,5000000.00,15000000.00);
+
+
+select gender, avg(net_pay) from 
+(employee inner join payroll on employee.employee_id=payroll.employee_id)
+group by gender;
+
+
+
 
